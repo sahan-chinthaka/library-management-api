18c50d583460a929fd2946253d680989d6f7c1d0
sahan-chinthaka <sahanchinthaka04@gmail.com>
2025-01-13 11:15:19 +0530

    Add DashboardController with GetBookCount action

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using library_management_api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_management_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly LibraryDBContext dbContext;
+
+        public DashboardController(LibraryDBContext context) => dbContext = context;
+
+        [HttpGet]
+        public IActionResult GetBookCount()
+        {
+            var bookCount = dbContext.Books.Count();
+            return Ok(bookCount);
+        }
+    }
+}
